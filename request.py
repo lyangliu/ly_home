5872c7c4272366578f8e61189353389d7a57ab67
lyang <benlang0612@163.com>
2019-05-18 11:15:44 +0800

    wrote a new file

---
+++ request.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+#_*_ UTF-8 _*_
+
+import requests
+import json
+
+'''
+response = requests.get('http://httpbin.org')
+print(type(response))
+print(response.status_code)
+print(type(response.text))
+print(response.text)
+print(response.cookies)
+'''
+
+'''
+requests.post('http://httpbin.org/post')
+requests.put('http://httpbin.org/put')
+requests.delete('http://httpbin.org/delete')
+requests.head('http://httpbin.org/get')
+request.optional('http://htttpbin.org/get')
+
+'''
+'''
+response = requests.get('http://httpbin.org')      #请求命令
+print(response.text)
+
+
+response = requests.get('http://httpbin.org/get?name=germey&age=22')   #带参数的请求命令方法一
+print(response.text)
+
+data = {
+        name:germey
+        age:22
+        }
+response = requests.get('http://httpbin.org/get',params = data)   #带参数的请求命令方法二
+print(response.text)
+
+'''
+
+response = requests.get('http://httpbin.org/get')
+print(type(response.text))
+print(response.json())
+print(json.loads(response.text))
+print(type(response.json()))
+
+
+
